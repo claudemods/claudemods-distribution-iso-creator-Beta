fe494c00bbe42a89db4973ff14272f12e33d6248
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-28 11:20:42 +0000

    having file system errors i think this is the fix

--- main.cpp
+++ main.cpp
@@ -164,7 +164,7 @@ private:
         std::cout << "██║░░██╗██║░░░░░██╔══██║██║░░░██║██║░░██║██╔══╝░░██║╚██╔╝██║██║░░██║██║░░██║░╚═══██╗" << std::endl;
         std::cout << "╚█████╔╝███████╗██║░░██║╚██████╔╝██████╔╝███████╗██║░╚═╝░██║╚█████╔╝██████╔╝██████╔╝" << std::endl;
         std::cout << "░╚════╝░╚══════╝╚═╝░░░░░░╚═════╝░╚═════╝░╚══════╝╚═╝░░░░░╚═╝░╚════╝░╚═════╝░╚═════╝░" << std::endl;
-        std::cout << COLOR_CYAN << "claudemods distribution iso creator Beta v1.0 19-11-2025" << COLOR_RESET << std::endl;
+        std::cout << COLOR_CYAN << "claudemods distribution iso creator Beta v1.0 28-11-2025" << COLOR_RESET << std::endl;
         std::cout << std::endl;
     }
 
@@ -437,6 +437,10 @@ private:
 
         // Copy calamares config
         execute_command("sudo cp -r " + currentDir + "/calamares-files/calamares " + target_folder + "/etc/");
+
+        execute_command("sudo cp -r " + currentDir + "11-dm-initramfs.rules " + target_folder + "/usr/lib/initcpio/udev/11-dm-initramfs.rules");
+
+        execute_command("sudo cp -r " + currentDir + "11-dm-initramfs.rules /usr/lib/initcpio/udev/11-dm-initramfs.rules");
 
         // Copy custom branding
         execute_command("sudo cp -r " + currentDir + "/calamares-files/claudemods " + target_folder + "/usr/share/calamares/branding/");
